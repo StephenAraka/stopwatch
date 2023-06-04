655f037a0efce0efb4e2b126279c96a4fb946682
Stephen Araka <stephen@Stephens-MacBook-Pro.local>
2023-06-04 12:35:30 +0300

    Add boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import { useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [timer, setTimer] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const countRef = useRef(null);
+
+  const startTimer = () => {
+    // start logic here
+  };
+
+  const pauseTimer = () => {
+    // pause logic here
+  }
+
+  const resumeTimer = () => {
+    // resume logic here
+  }
+
+  const resetTimer = () => {
+    // reset  logic here
+  }
 
   return (
     <>
-      <div>yo</div>
+      <h1>Stopwatch</h1>
+      <div>00</div>
+      <div>
+        <button>Start</button>
+        <button>Pause</button>
+        <button>Resume</button>
+        <button>Reset</button>
+      </div>
     </>
   )
 }
